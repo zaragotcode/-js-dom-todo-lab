dc4ebcc180e16f8b45ba9ac7e41ccaa6728d8111
Patrick Mazara <patrickmazara@hotmail.com>
2022-12-08 00:13:37 -0500

    add styling and functionality to button event listener

--- js/app.js
+++ js/app.js
@@ -5,11 +5,12 @@ const inputEl = document.querySelector("#list-input")
 const buttonEl = document.querySelector("#submit-button")  
 const ulEl = document.querySelector("#todo-list") 
 
-// add event listener that adds value to unordered list
-// on click and resets the input id 
+// add event listener that adds value to unordered list on click
 buttonEl.addEventListener("click", function(evt) {
     const li = document.createElement("li") 
     const inpEl = document.querySelector("input")
     li.textContent = inpEl.value  
     ulEl.appendChild(li) 
+    return inpEl.value = "" 
+    
 })
